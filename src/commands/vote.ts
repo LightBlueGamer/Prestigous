fbf054e8e5aa0240e5f021ecf1b2bc3b13bd543d
LightBlueGamer <timjonsson03@hotmail.com>
2022-07-24 19:50:26 +0200

    minor change

--- src/commands/vote.ts
+++ src/commands/vote.ts
@@ -8,7 +8,7 @@ export default {
     async execute(interaction: CommandInteraction) {
         const ping = (await getProfile(interaction.user.id)).ping;
         interaction.reply({
-            content: `You can vote for the bot on TopGG here: https://top.gg/bot/994973502975262980/vote`,
+            content: `You can vote for the bot on TopGG here: <https://top.gg/bot/994973502975262980/vote>`,
             allowedMentions: {
                 repliedUser: ping,
             },
