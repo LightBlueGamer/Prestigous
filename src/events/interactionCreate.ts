ed88b69e99ba4b06a13181f7d516d9ed2dec69d9
LightBlueGamer <timjonsson03@hotmail.com>
2022-07-27 20:43:39 +0200

    fixed channel config

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -30,7 +30,7 @@ export default {
             if(interaction.commandName === 'configuration') {
                 choices = interaction.guild?.channels.cache.filter(x => x.isTextBased()).map(x => ({name: `${x.parent ? `${x.parent.name}/` : ''}${x.name}`, value: x.id}));
                 choices!.push({name: 'Current', value: 'current'});
-                const filtered = choices?.filter(choice => choice.name.split('/')[0].toLowerCase().startsWith(focusedValue.toLowerCase())).slice(0, 25);
+                const filtered = choices?.filter(choice => choice.name.split('/')[1].toLowerCase().startsWith(focusedValue.toLowerCase())).slice(0, 25);
                 return await interaction.respond(filtered?.map(choice => ({ name: choice.name, value: choice.value }))!);
             }
 
